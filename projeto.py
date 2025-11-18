6db40ab86ccd31bfd60abfe76ea73bc2e4a01d42
Paulerou <143561697+Paulerou@users.noreply.github.com>
2025-11-18 19:54:38 -0300

    FuncaoSimplex

--- projeto.py
+++ projeto.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Sistema de Otimizacao Automatica - Deteccao de Tipos
+Sistema de Otimizacao Automatica. - Deteccao de Tipos
 Autor: Sistema de Otimizacao Inteligente
 """
 
